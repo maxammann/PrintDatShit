f709325e18271505b96a1022358481d7b16d3edf
p000ison <maximilian.ammann@googlemail.com>
2014-09-20 13:27:37 +0200

    Initial commit

---
+++ print_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PRINTER=$1
+OUTPUT=$2
+
+mkdir $OUTPUT
+find . -type f -name "*.doc" -exec libreoffice --headless --print-to-file --printer-name the_printer_name --outdir O$UTPUT {} \;
+
+for f in $OUTPUT/*
+do
+  echo "Printing " $f "?"
+  read
+  lp -d $PRINTER $f
+done
