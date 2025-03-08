50da548c10f2bcf298e1d142068b896e4c215054
Max Ammann <max@maxammann.org>
2025-03-08 20:11:15 +0100

    add random changes

--- print_docs.sh
+++ print_docs.sh
@@ -3,7 +3,7 @@ PRINTER=$1
 OUTPUT=$2
 
 mkdir $OUTPUT
-find . -type f -name "*.doc" -exec libreoffice --headless --print-to-file --printer-name the_printer_name --outdir O$UTPUT {} \;
+find . -type f -name "*.doc" -exec libreoffice --headless --print-to-file --printer-name the_printer_name --outdir $OUTPUT {} \;
 
 for f in $OUTPUT/*
 do
